060e5ce7de892c170b381dcc1e69f20b605700fa
Decoder Ashish <decoderashish@gmail.com>
2024-05-17 12:07:32 +0530

    added eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-    apps: [
-      {
-        name: 'boilerplate',
-        script: 'server.js',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '1G',
-        env: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: 'boilerplate',
+      script: 'server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
       },
-    ],
-  };
-  
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
